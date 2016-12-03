e614964ef909c4cd569004f3b009107950fc3cff
yotoo2920 <you@example.com>
2016-12-03 14:56:48 -0500

    update the urls

--- db_proj/urls.py
+++ db_proj/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
-    url(r'^$', views.home_page, name='set_of_data'),
-    url(r'^$', views.home_page, name='queries'),
-    url(r'^$', views.home_page, name='about_us'),
+    url(r'^$', views.set_of_data, name='set_of_data'),
+    url(r'^$', views.queries, name='queries'),
+    url(r'^$', views.about_us, name='about_us'),
 ]
