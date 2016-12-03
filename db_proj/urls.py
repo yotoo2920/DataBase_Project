9930234514c566ae35d0c0e6305a8eed307f9111
yotoo2920 <you@example.com>
2016-12-03 16:08:28 -0500

    added some views

--- db_proj/urls.py
+++ db_proj/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
-    url(r'^$', views.set_of_data, name='set_of_data'),
+    url(r'^set_of_data/$', views.set_of_data, name='set_of_data'),
     url(r'^$', views.queries, name='queries'),
     url(r'^$', views.about_us, name='about_us'),
 ]
+# ^students/$
--- db_proj/views.py
+++ db_proj/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import City
 from .models import Person
+from .models import
 
 def home_page(request):
 	city = City.objects.using('default').get(name="Gainesville")
 	return render(request, 'db_proj/home_page.html', { 'city': city })
 
 def set_of_data(request):
-	person = Person.objects.using('default').get(region="MA")
-	return render(request, 'db_proj/set_of_data.html', { 'person': person})
+	# person = Person.objects.using('default').get(region="MA  ")
+	# return render(request, 'db_proj/set_of_data.html', { 'person': person})
+	city = City.objects.using('default').get(name="Gainesville")
+	return render(request, 'db_proj/home_page.html', { 'city': city })
+
+def queries(request):
+	city = City.objects.using('default').get(name="Gainesville")
+	return render(request, 'db_proj/queries.html', { 'city': city })
