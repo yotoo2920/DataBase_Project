4a0ea5ce679f078772581147c3099bf7384e6486
yotoo2920 <you@example.com>
2016-12-03 18:53:04 -0500

    Adding new CSS, not working yet

--- db_proj/urls.py
+++ db_proj/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
     url(r'^set_of_data/$', views.set_of_data, name='set_of_data'),
-    url(r'^$', views.queries, name='queries'),
-    url(r'^$', views.about_us, name='about_us'),
+    url(r'^queries/$', views.queries, name='queries'),
+    url(r'^about_us/$', views.about_us, name='about_us'),
 ]
 # ^students/$
--- db_proj/views.py
+++ db_proj/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import City
 from .models import Person
-from .models import
+from .models import Occupation
+from .models import Language
 
 def home_page(request):
 	city = City.objects.using('default').get(name="Gainesville")
 	return render(request, 'db_proj/home_page.html', { 'city': city })
 
 def set_of_data(request):
-	# person = Person.objects.using('default').get(region="MA  ")
-	# return render(request, 'db_proj/set_of_data.html', { 'person': person})
-	city = City.objects.using('default').get(name="Gainesville")
+	city = City.objects.using('default').get(name="Atlanta")
 	return render(request, 'db_proj/home_page.html', { 'city': city })
 
 def queries(request):
-	city = City.objects.using('default').get(name="Gainesville")
+	city = City.objects.using('default').get(name="Miami")
 	return render(request, 'db_proj/queries.html', { 'city': city })
+
+def about_us(request):
+	city = City.objects.using('default').get(name="Boston")
+	return render(request, 'db_proj/about_us.html', { 'city': city })
