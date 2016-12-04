a2ae3c4c31cf61605f251df1f9b8217fffa77e02
Dario <yotoo2920@yahoo.com>
2016-12-04 18:16:11 -0500

    trying to add sql queries

--- db_proj/views.py
+++ db_proj/views.py
@@ -4,6 +4,7 @@ from .models import City
 from .models import Person
 from .models import Occupation
 from .models import Language
+from django.db.models import Avg
 
 def home_page(request):
 	city = City.objects.using('default').get(name="Gainesville")
@@ -14,8 +15,10 @@ def set_of_data(request):
 	return render(request, 'db_proj/home_page.html', { 'city': city })
 
 def queries(request):
-	city = City.objects.using('default').get(name="Miami")
-	return render(request, 'db_proj/queries.html', { 'city': city })
+	sal = Person.objects.using('default').aggregate(Avg('weekly_salary'))
+	return render(request, 'db_proj/queries.html', { 'Avg Weekly Salary': sal })
+	# person = Person.objects.raw('SELECT  TRUNC(AVG(WEEKLY_SALARY)*12, 2) AS AVERAGE_SALARY FROM PERSON')
+	# return render(request, 'db_proj/queries.hmtl', {'AVERAGE_SALARY' : person})
 
 def about_us(request):
 	city = City.objects.using('default').get(name="Boston")
