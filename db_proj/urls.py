88e17e3b486de9bd3347e14cb870ba6429827368
yotoo2920 <you@example.com>
2016-12-04 20:00:14 -0500

    added queries 1-4

--- db_proj/urls.py
+++ db_proj/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     url(r'^set_of_data/$', views.set_of_data, name='set_of_data'),
     url(r'^queries/$', views.queries, name='queries'),
     url(r'^about_us/$', views.about_us, name='about_us'),
+    url(r'^queries/query1$', views.query1, name='query1'),
+    url(r'^queries/query2$', views.query2, name='query2'),
+    url(r'^queries/query3$', views.query3, name='query3'),
+    url(r'^queries/query4$', views.query4, name='query4'),
+    url(r'^queries/query5$', views.query5, name='query5'),
 ]
--- db_proj/views.py
+++ db_proj/views.py
@@ -6,6 +6,8 @@ from .models import Occupation
 from .models import Language
 from django.db.models import Avg
 
+from django.db import connection
+
 def home_page(request):
 	city = City.objects.using('default').get(name="Gainesville")
 	return render(request, 'db_proj/home_page.html', { 'city': city })
@@ -17,9 +19,48 @@ def set_of_data(request):
 def queries(request):
 	sal = Person.objects.using('default').aggregate(Avg('weekly_salary'))
 	return render(request, 'db_proj/queries.html', { 'Avg Weekly Salary': sal })
-	# person = Person.objects.raw('SELECT  TRUNC(AVG(WEEKLY_SALARY)*12, 2) AS AVERAGE_SALARY FROM PERSON')
-	# return render(request, 'db_proj/queries.hmtl', {'AVERAGE_SALARY' : person})
+
+def query1(request):
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT  TRUNC(AVG(WEEKLY_SALARY)*12, 2) AS AVERAGE_SALARY FROM PERSON")
+		row = cursor.fetchone()
+
+	return render(request, 'db_proj/query1.html', { 'average': row[0] })
+
+def query2(request):
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT OCCUPATION, TRUNC(AVG(WEEKLY_SALARY)*12, 2) AS AVERAGE_SALARY FROM PERSON WHERE WEEKLY_SALARY = (SELECT MAX(WEEKLY_SALARY) FROM PERSON) AND ROWNUM = 1 GROUP BY OCCUPATION ")
+		rows = dictfetchall(cursor)
+
+	return render(request, 'db_proj/query2.html', { 'rows': rows })
+
+def query3(request):
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT * FROM (SELECT STATE.NAME AS STATE, TRUNC(AVG(WEEKLY_SALARY)*12, 2) AS AVERAGE_SALARY FROM STATE JOIN PERSON ON STATE.ABBREVIATION = PERSON.REGION WHERE ROWNUM <= 15 GROUP BY STATE.NAME) ORDER BY AVERAGE_SALARY DESC")
+		rows = dictfetchall(cursor)
+
+	return render(request, 'db_proj/query3.html', { 'rows': rows })
+
+def query4(request):
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT * FROM(SELECT NAME, TRUNC(GDP/POPULATION,2) AS WEALTH_PER_CITIZEN FROM(SELECT STATE.NAME AS NAME, COUNT(PERSON.NAME) AS POPULATION, SUM(PERSON.WEEKLY_SALARY)*12 AS GDP FROM PERSON JOIN STATE ON STATE.ABBREVIATION = PERSON.REGION GROUP BY STATE.NAME) ORDER BY WEALTH_PER_CITIZEN DESC) WHERE ROWNUM < 6")
+		rows = dictfetchall(cursor)
+
+	return render(request, 'db_proj/query4.html', { 'rows': rows })
+
+
+def query5(request):
+	city = City.objects.using('default').get(name="Miami")
+	return render(request, 'db_proj/queries.html', { 'city': city })
 
 def about_us(request):
 	city = City.objects.using('default').get(name="Boston")
 	return render(request, 'db_proj/about_us.html', { 'city': city })
+
+def dictfetchall(cursor):
+	"Return all rows from a cursor as a dict"
+	columns = [col[0] for col in cursor.description]
+	return [
+		dict(zip(columns, row))
+		for row in cursor.fetchall()
+	]
